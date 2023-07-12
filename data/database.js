c0eb311297a6039eb9507d6c7a0df66f131f724a
Pintu Kumar <pintukumar03072002@gmail.com>
2023-07-12 17:49:13 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -1,17 +1,15 @@
 import mongoose from "mongoose";
 
 export const connectDB = () => {
-  // mongoose
-  //   .connect(process.env.MONGO_URI,
-  //   //    {
-  //   //   dbName: "backendapi",
-  //   // }
-  //   )
-  //   .then((c) => console.log(`Database Connected with ${c.connection.host}`))
-  //   .catch((e) => console.log(e));
-  mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log(`Database connected successfully`);
-  })
+  mongoose
+    .connect(process.env.MONGO_URI,
+       {
+      dbName: "backendapi",
+    }
+    )
+    .then((c) => console.log(`Database Connected with ${c.connection.host}`))
+    .catch((e) => console.log(e));
+
 
 };
 
